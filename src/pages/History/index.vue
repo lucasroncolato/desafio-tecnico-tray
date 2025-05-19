60e78a7c21eac7a24f8168debba8174544aebba4
Lucas <lucas_bezerraroncolato@hotmail.com>
2025-05-19 10:42:45 -0300

    Atualizar layout

--- src/pages/History/index.vue
+++ src/pages/History/index.vue
@@ -1,29 +1,35 @@
 <template>
-  <Background>
+  <div class="content">
     <div class="p-6 from-gray-100 to-gray-300 dark:from-gray-900 dark:to-black text-gray-800 dark:text-gray-100">
-      <h2 class="text-2xl font-bold mb-4">Histórico de Partidas</h2>
+      <div class="flex items-center justify-center content-center mb-4">
+        <BaseButton @click="goBack" class="text-sm pr-4 cursor-pointer">
+          ←
+        </BaseButton>
+        <h2 class="text-2xl font-bold">Histórico de Partidas</h2>
+      </div>
+
       <div v-if="loading">Carregando...</div>
       <div v-else-if="games.length === 0">Nenhum jogo encontrado.</div>
       <ul v-else class="space-y-4">
-        <li
-          v-for="game in games"
-          :key="game.id"
-          class="p-4 bg-white dark:bg-gray-800 rounded shadow-md"
-        >
+        <li v-for="game in games" :key="game.id" class="p-4 bg-white dark:bg-gray-800 rounded shadow-md">
           <p><strong>Tempo:</strong> {{ game.time }}s</p>
           <p><strong>Tentativas:</strong> {{ game.attempts }}</p>
           <p><strong>Data:</strong> {{ new Date(game.date).toLocaleString() }}</p>
         </li>
       </ul>
     </div>
-  </Background>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Background from "@/components/elements/Background/index.vue";
+
 import { ref, onMounted } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goBack = () => router.back()
 
 const auth = useAuthStore()
 const games = ref<any[]>([])
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,css}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
